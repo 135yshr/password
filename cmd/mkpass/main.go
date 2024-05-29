b463ca63f7434c26a77eba7d06b0cfabbca41139
135yshr <isago@oreha.dev>
2024-05-29 20:42:23 +0900

    :sparkles: Add print version

--- cmd/mkpass/main.go
+++ cmd/mkpass/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"runtime"
 	"strconv"
 
 	"github.com/135yshr/password"
@@ -13,26 +14,46 @@ import (
 const defaultLength = 12
 
 var (
+	version string
+	commit  string
+	date    string
+	builtBy string
+)
+
+var (
 	length int
 	upper  bool
 	lower  bool
 	number bool
 	symbol bool
 	help   bool
+	ver    bool
 )
 
-func main() {
+func initFlags() {
 	flag.IntVar(&length, "length", defaultLength, "length of password")
 	flag.BoolVar(&upper, "upper", true, "use uppercase")
 	flag.BoolVar(&lower, "lower", true, "use lowercase")
 	flag.BoolVar(&number, "number", true, "use numbers")
 	flag.BoolVar(&symbol, "symbol", true, "use symbols")
 	flag.BoolVar(&help, "help", false, "show help")
+	flag.BoolVar(&ver, "version", false, "show version")
+}
 
+func main() {
+	initFlags()
 	flag.Parse()
 
+	// show help
 	if help {
+		printVersion()
 		flag.Usage()
+		os.Exit(0)
+	}
+
+	// show version
+	if ver {
+		printVersion()
 		os.Exit(0)
 	}
 
@@ -55,6 +76,19 @@ func main() {
 	for range count {
 		fmt.Println(gen.Generate(length))
 	}
+}
+
+func printVersion() {
+	fmt.Printf("%s - generate passwords\n", os.Args[0])
+	fmt.Printf("https://github.com/135yshr/password\n\n")
+	fmt.Printf("Version:\t%s\n", version)
+	fmt.Printf("Commit:\t\t%s\n", commit)
+	fmt.Printf("BuildDate:\t%s\n", date)
+	fmt.Printf("BuiltBy:\t%s\n", builtBy)
+	fmt.Printf("GoVersion:\t%s\n", runtime.Version())
+	fmt.Printf("GoOS:\t\t%s\n", runtime.GOOS)
+	fmt.Printf("GoArch:\t\t%s\n", runtime.GOARCH)
+	fmt.Println("\nMIT License (c) 2024 135yshr")
 }
 
 func createPolicies() []password.Policy {
